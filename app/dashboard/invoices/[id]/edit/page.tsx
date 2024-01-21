2777e3640be361cc645119479ed168cb983e5265
Mihai Bancila <68776490+mihai133@users.noreply.github.com>
2024-01-21 12:03:17 +0200

    Chapter 15 - Auth

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import EditInvoiceForm from '@/app/ui/invoices/edit-form';
+import { notFound } from 'next/navigation';
 
 import React from 'react'
 
@@ -10,6 +11,10 @@ export default async function page({params}: { params: {id: string}}) {
     fetchInvoiceById(id),
     fetchCustomers()
   ])
+
+  if(!invoice) {
+    notFound()
+  }
 
   return (
     <main>
